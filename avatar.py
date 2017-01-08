4285482df7ac125482d529cb16d6a7101bb54850
lgd20 <lgdegraw@colby.edu>
2017-01-08 17:46:22 -0500

    Update avatar.py

--- avatar.py
+++ avatar.py
@@ -2,14 +2,15 @@
 # Jan Plan 2017
 # CS269
 # 1/5/17
+import pygame
 
 class Avatar():
 
-	def __init__(self, position, yVelocity, color):
+	def __init__(self, position, yVelocity, color, x1, y1, x2, y2):
 		self.position = position
 		self.vy = yVelocity
 		self.color = color
-		
+		flyer = pygame.Rect((x1, y1), (x2, y2))
 	def setPosition(self, position):
 		self.position = position
 		
