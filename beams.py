702ad3a38e737baa12cc23d001de6aa80bf78389
lgd20 <lgdegraw@colby.edu>
2017-01-08 17:47:12 -0500

    Update beams.py

--- beams.py
+++ beams.py
@@ -1,8 +1,9 @@
 class Beams():
 
-	def __init__(self, position, color):
+	def __init__(self, position, color, x1, y1, x2, y2):
 		self.position = position
 		self.color = color
+		beam = pygame.Rect((x1, y1), (x2, y2))
 		
 	def setPosition(self, position):
 		self.position = position
