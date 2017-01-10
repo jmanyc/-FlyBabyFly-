110f44cb72620033358b5ac7b88885396adeb278
lgd20 <lgdegraw@colby.edu>
2017-01-10 17:31:24 -0500

    Finished obstacles and wall classes, plus test files for each. Obstacles have an optional keyword argument for the speed; it can take in a speed that will remain constant (in which case the ‘speed’ argument in moveObs would need to be changed to self.speed), or its speed can be changed depending on what is passed into moveObs (in the Obstacles class) from moveWall (in the Wall class).
    The first argument passed into Wall when creating the wall object is
    the color of the preceding beam. For testing purposes, I passed in
    BLUE, although it is irrelevant at this point because the wall colors
    will be assigned in the main loop based on the beam color.

--- obstacles.py
+++ obstacles.py
@@ -7,7 +7,7 @@ import pygame, os, sys
 
 class Obstacle():
 
-	def __init__(self, position, color, width, height, speed):
+	def __init__(self, position, color, width, height, speed = None):
 	
 		self.position = position
 		self.x = self.position[0]
@@ -37,41 +37,6 @@ class Obstacle():
 	def moveObs(self, speed, surface):
 
 		surface.fill((255, 255, 255), self.obstacle)	# fill a surface with the obstacle on a white background
-		self.obstacle.move_ip(self.speed, 0)	# change the object's internal position
+		self.obstacle.move_ip(speed, 0)	# change the object's internal position
 
 		self.draw(surface)    # redraw the obstacle at its new position on the display
-		
-		
-# ------------- Test code ----------------------------------------------------------------
-		
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-
-
-# color options 
-
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0,0,255)
-
-myObstacle = Obstacle([500, 500], BLUE, 40, 280, -5)	# create the obstacle object
-
-clock = pygame.time.Clock()	# initialize pygame's internal clock
-screen.fill((255,255,255))	# fill the screen with a white background
-
-
-myObstacle.draw(screen)	# draw the obstacle to the screen
-print "entering main loop"
-
-while 1: #Main loop
-
-	myObstacle.moveObs(-5, screen)	# move the obstacle leftwards
-	
-	pygame.display.update([myObstacle.obstacle]) # update the location of the obstacle on the screen
-	
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			print "terminating"
-			sys.exit()
-			break
-	clock.tick(60) # 60 fps
--- wall.py
+++ wall.py
@@ -1,29 +1,76 @@
-import obstacles
+# Fly Baby Fly
+# Jan Plan 2017
+# CS269
+# 1/5/17
+
+import pygame, os, sys
+from obstacles import Obstacle
+
 
 class Wall(Obstacle):
 	
-	def __init__(self, lastBeamColor):
-	
-		Obstacle.__init__(self, position, color, x1, y1, x2, y2)
-		self.b1color = b1color
-		self.b2color = b2color
-		self.b3color = b3color
+	def __init__(self, lastBeamColor, pos1, pos2, pos3, color1, color2, color3, heights, speed = None):
+
+		self.width = 40 # width hardcoded because it should always be the same
+		self.heights = heights
+		self.speed = speed
+		if self.speed != None:
+			self.section1 = Obstacle(pos1, color1, self.width, self.heights[0], self.speed)
+			self.section2 = Obstacle(pos2, color2, self.width, self.heights[1], self.speed)
+			self.section3 = Obstacle(pos3, color3, self.width, self.heights[2], self.speed)
+		else:
+			self.section1 = Obstacle(pos1, color1, self.width, self.heights[0])
+			self.section2 = Obstacle(pos2, color2, self.width, self.heights[1])
+			self.section3 = Obstacle(pos3, color3, self.width, self.heights[2])
+		
+		self.wallSections = []
+		self.wallSections.extend([self.section1.obstacle, self.section2.obstacle, self.section3.obstacle])
+		
+		self.sub_wallSections = []
+		self.sub_wallSections.extend([self.section1, self.section2, self.section3])
+		
+		self.section1color = color1
+		self.section2color = color2
+		self.section3color = color3
 		self.lastBeamColor = lastBeamColor
 		
-	def setb1color(self, color):
-		self.b1color = b1color
+		self.sectionColors = []
+		self.sectionColors.extend([self.section1color, self.section2color, self.section3color])
 		
-	def getb1color(self):
-		return self.b1color
+	def setSection1color(self, color):
+		self.section1color = color
 		
-	def setb2color(self, color):
-		self.b2color = b1color
+	def getSection1color(self):
+		return self.section1color
 		
-	def getb2color(self):
-		return self.b2color
+	def setSection2color(self, color):
+		self.section2color = color
 		
-	def setb3color(self, color):
-		self.b3color = b1color
+	def getSection2color(self):
+		return self.section2color
 		
-	def getb3color(self):
-		return self.b3color
+	def setSection3color(self, color):
+		self.section3color = color
+		
+	def getSection3color(self):
+		return self.section3color
+		
+	def draw(self, surface):
+		pygame.draw.rect(surface, self.section1color, self.section1.obstacle)
+		pygame.draw.rect(surface, self.section2color, self.section2.obstacle)
+		pygame.draw.rect(surface, self.section3color, self.section3.obstacle)
+
+		
+	def moveWall(self, speed, surface):
+		for section in self.sub_wallSections:
+			section.moveObs(speed, surface)
+			section.draw(surface)
+			
+		self.update()
+		
+	def update(self):
+		for section in self.wallSections:
+			pygame.display.update([section])
+			
+	def sectionList(self):
+		return self.wallSections
