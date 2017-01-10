2f0a779a52ccb59102958e2b8dcdc766956d4ca4
lgd20 <lgdegraw@colby.edu>
2017-01-10 12:52:29 -0500

    Update obstacles.py

--- obstacles.py
+++ obstacles.py
@@ -1,9 +1,23 @@
+# Fly Baby Fly
+# Jan Plan 2017
+# CS269
+# 1/10/17
+
+import pygame, os, sys
+
 class Obstacle():
 
-	def __init__(self, position, color, x1, y1, x2, y2):
+	def __init__(self, position, color, width, height, speed):
+	
 		self.position = position
+		self.x = self.position[0]
+		self.y = self.position[1]
 		self.color = color
-		obstacle = pygame.Rect((x1, y1), (x2, y2))
+		self.speed = speed
+		self.height = height
+		self.width = width
+		self.obstacle = pygame.Rect((self.x-self.width/2,self.y-self.height/2), (self.width, self.height))
+		
 		
 	def setPosition(self, position):
 		self.position = position
@@ -16,3 +30,48 @@ class Obstacle():
 		
 	def getColor(self):
 		return self.color
+	
+	def draw(self, surface):
+		pygame.draw.rect(surface, self.color, self.obstacle)
+				
+	def moveObs(self, speed, surface):
+
+		surface.fill((255, 255, 255), self.obstacle)	# fill a surface with the obstacle on a white background
+		self.obstacle.move_ip(self.speed, 0)	# change the object's internal position
+
+		self.draw(surface)    # redraw the obstacle at its new position on the display
+		
+		
+# ------------- Test code ----------------------------------------------------------------
+		
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+
+
+# color options 
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0,0,255)
+
+myObstacle = Obstacle([500, 500], BLUE, 40, 280, -5)	# create the obstacle object
+
+clock = pygame.time.Clock()	# initialize pygame's internal clock
+screen.fill((255,255,255))	# fill the screen with a white background
+
+
+myObstacle.draw(screen)	# draw the obstacle to the screen
+print "entering main loop"
+
+while 1: #Main loop
+
+	myObstacle.moveObs(-5, screen)	# move the obstacle leftwards
+	
+	pygame.display.update([myObstacle.obstacle]) # update the location of the obstacle on the screen
+	
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			print "terminating"
+			sys.exit()
+			break
+	clock.tick(60) # 60 fps
